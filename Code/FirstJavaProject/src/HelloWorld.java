5234760ec4855b9fdfb987a04aaec32739de444e
Florin.Teo <Florin.Teo@live.com>
2020-03-01 11:22:14 -0800

    Adding nested loops

--- Code/FirstJavaProject/src/HelloWorld.java
+++ Code/FirstJavaProject/src/HelloWorld.java
@@ -2,7 +2,11 @@
 public class HelloWorld {
 
 	public static void snakeVertical(int nRows, int nCols) {
-		
+		for(int r = 0; r < nRows; r++) {
+			for (int c = 0; c < nCols; c++) {
+				
+			}
+ 		}
 	}
 	
 	public static void main(String[] args) {
